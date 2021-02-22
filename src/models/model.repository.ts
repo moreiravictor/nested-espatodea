7279ebc81bfbd0b48d910c8ba1b27e214c35291f
victor <victormoreira176@hotmail.com>
2021-02-22 01:07:46 -0300

    :hammer:pontual fixes at model.repository

--- src/models/model.repository.ts
+++ src/models/model.repository.ts
@@ -7,19 +7,20 @@ import { ObjectID } from 'mongodb';
 
 
 export class ModelRepository<T, K extends Model> extends Repository<T> {
+
     async get(id: string, relations: string[] = [], throwsException = true): Promise<K | null> {
       const entity = await this.findOne({where: { _id: new ObjectID(id) }, relations})
       if (!entity && throwsException)
         return Promise.reject(new NotFoundException('Model not found.'))
         
-      return Promise.resolve(entity ? this.transform(entity) : null)
+      return Promise.resolve(this.transform(entity))
     }
     
     async createEntity(inputs: DeepPartial<T>,relations: string[] = []): Promise<K> {
       const saved = await this.save(inputs)
       if (relations.length)
         return await this.get((saved as any)._id, relations)
-      return this.transform(saved)
+      return Promise.resolve(this.transform(saved))
     }
 
     async updateEntity(id: string, inputs: QueryDeepPartialEntity<T>, relations: string[] = []): Promise<K> {
